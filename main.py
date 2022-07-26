ac9d9b97a049093ab2f95ad4e3e298eaee51b1e3
fschn <fschnabel007@gmail.com>
2022-07-26 22:20:03 +0200

    Add errorhandling and add commandline argument to get the user

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+import os
+import sys
+
 import requests
 
-def main():
-    req = requests.post('https://www17.muenchen.de/Passverfolgung/', data={'ifNummer': '', 'pbAbfragen' : 'Abfragen'})
-    if('noch nicht zur Abholung bereit.' in req.text):
+def main(passport_id: str):
+    req = requests.post('https://www17.muenchen.de/Passverfolgung/', data={'ifNummer': passport_id, 'pbAbfragen' : 'Abfragen'})
+    if 'noch nicht zur Abholung bereit.' in req.text:
         print('Noch nicht zur Abholung bereit.')
+    elif 'Fehler:' in req.text:
+        print(req.text)
+        raise Exception('Fehler beim Abrufen der Passverfolgung.')
     else:
         print('Zur Abholung bereit.')
         raise Exception('Zur Abholung bereit.')
 
 if __name__ == '__main__':
-    main()
+    print(sys.argv)
+    if len(sys.argv) < 2:
+        print('Usage: python main.py <User>')
+        sys.exit(1)
+    user = sys.argv[1]
+
+    if user == 'FELIX':
+        main(os.environ['FELIX_PASSPORT_ID'])
+    elif user == 'CHRISSI':
+        main(os.environ['CHRISSI_PASSPORT_ID'])
+    else:
+        print('Usage: python main.py <User>')
+        sys.exit(1)
